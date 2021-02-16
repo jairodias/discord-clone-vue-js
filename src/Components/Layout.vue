78636c1b7fdabb7944de30cb317dab5ac9ac04a1
jairodias <jairopereira_dias@hotmail.com>
2021-02-16 18:18:37 -0300

    [master] - Começando a componentizaçao da aplicaçao

---
+++ src/Components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="grid">
+        <ServeList />
+    </div>
+</template>
+
+<script>
+
+import ServeList from "./ServeList/Index.vue";
+
+export default {
+  components: { ServeList }
+}
+</script>
+
+<style scoped>
+    #grid {
+        display: grid;
+        height: 100vh;
+        grid-template-columns: 71px 240px auto 240px;
+        grid-template-rows: 46px auto 52px;
+        grid-template-areas: 
+            "SL SN CI CI"
+            "SL CL CD UL"
+            "SL UI CD UL"
+        ;
+    }
+</style>
---
+++ src/Components/ServeList/Index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <ServerButton isHome /> 
+
+        <div class="separator"></div>
+
+        <ServerButton :mentions="3"/>
+        <ServerButton />
+        <ServerButton hasNotification />
+        <ServerButton :mentions="36" hasNotification />
+        <ServerButton />
+        <ServerButton />
+        <ServerButton  :mentions="41" hasNotification/>
+
+    </div>
+</template>
+<script>
+
+import ServerButton from "./ServerButton"
+
+export default {
+    components: {ServerButton}
+}
+</script>
+<style scoped lang="scss">
+    .container {
+        grid-area: SL;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: var(--tertiary);
+
+        padding: 11px 0;
+        max-height: 100vh;
+        transition: .2s;
+    }
+
+    .separador {
+        width: 32px;
+        border-bottom: solid 2px var(--quartenary);
+        margin-bottom: 8px;
+    }
+</style>
